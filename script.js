4859db5405d7f546674254331a1b75a14eb54602
Januar Rama Fitra <49686801+januarramafitra@users.noreply.github.com>
2019-04-25 11:49:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var starwars = [
+    {
+        name: "Alderaan",
+        "rotation_period": "24",
+        "orbital_period": "364",
+    },
+    {
+        "name": "Yavin IV",
+        "rotation_period": "24",
+        "orbital_period": "4818",
+    },
+    {
+        "name": "Hoth",
+        "rotation_period": "23",
+        "orbital_period": "549",
+    },
+    {
+        "name": "Dagobah",
+        "rotation_period": "23",
+        "orbital_period": "341",
+    },
+    {
+        "name": "Bespin",
+        "rotation_period": "12",
+        "orbital_period": "5110",
+    },
+    {
+        "name": "Endor",
+        "rotation_period": "18",
+        "orbital_period": "402",
+    },
+    {
+        "name": "Naboo",
+        "rotation_period": "26",
+        "orbital_period": "312",
+    },
+    {
+        "name": "Coruscant",
+        "rotation_period": "24",
+        "orbital_period": "368",
+    },
+    {
+        "name": "Kamino",
+        "rotation_period": "27",
+        "orbital_period": "463",
+    },
+    {
+        "name": "Geonosis",
+        "rotation_period": "30",
+        "orbital_period": "256",
+    }
+]
